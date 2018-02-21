f961ac22908734cdfd4ea344e01089dde2d2f913
Christopher Torgalson <manager@bedlamhotel.com>
2018-02-21 22:43:21 +0100

    Adds many new kde rc & config files; removes deadwood

---
+++ .config/plasma-locale-settings.sh
@@ -0,0 +1,9 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=iu_CA.UTF-8
+export LC_NUMERIC=iu_CA.UTF-8
+export LC_TIME=iu_CA.UTF-8
+export LC_MONETARY=iu_CA.UTF-8
+export LC_MEASUREMENT=iu_CA.UTF-8
---
+++ .local/share/konsole/Solarized Dev.profile
@@ -0,0 +1,15 @@
+[Appearance]
+ColorScheme=Solarized
+Font=Monospace,12,-1,0,50,0,0,0,0,0,Regular
+
+[General]
+Name=Solarized Dev
+Parent=FALLBACK/
+
+[Interaction Options]
+AutoCopySelectedText=true
+TrimTrailingSpacesInSelectedText=true
+
+[Scrolling]
+HistorySize=1000
+ScrollBarPosition=2
