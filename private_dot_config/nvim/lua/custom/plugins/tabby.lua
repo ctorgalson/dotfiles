d37fb676ecd7c42541d92021acf0dde9a890b598
Christopher Torgalson <manager@bedlamhotel.com>
2024-01-12 10:26:05 +0100

    Random changes to tabby.lua; WIP

--- private_dot_config/nvim/lua/custom/plugins/tabby.lua
+++ private_dot_config/nvim/lua/custom/plugins/tabby.lua
@@ -5,11 +5,14 @@ return {
   config = function()
     -- configs...
     local theme = {
-      fill = 'TabLineFill',
+      -- fill = 'TabLineFill',
+      fill = { bg='#002b36', fg='#657b83' },
       -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
-      head = 'TabLine',
+      -- head = 'TabLine',
+      head = { bg='#002b36', fg='#657b83' },
       current_tab = 'TabLineSel',
-      tab = 'TabLine',
+      -- tab = 'TabLine',
+      tab = { bg='#002b36', fg='#657b83' },
       win = 'TabLine',
       tail = 'TabLine',
     }
@@ -22,13 +25,13 @@ return {
         line.tabs().foreach(function(tab)
           local hl = tab.is_current() and theme.current_tab or theme.tab
           return {
-            line.sep(' ', hl, theme.fill),
+            -- line.sep(' ', hl, theme.fill),
             -- tab.is_current() and '●' or '',
-            '',
+            ' ',
             tab.number(),
             tab.name(),
-            tab.close_btn('✕ '),
-            line.sep(' ', hl, theme.fill),
+            tab.close_btn('✕  '),
+            -- line.sep(' ', hl, theme.fill),
             hl = hl,
             margin = ' ',
           }
@@ -36,19 +39,22 @@ return {
         line.spacer(),
         line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
           return {
-            line.sep(' ', theme.win, theme.fill),
+            line.sep(' ', theme.tab, theme.fill),
             -- win.is_current() and '●' or '',
-            '',
+            ' ',
+            win.file_icon(),
+            ' ',
             win.buf_name(),
-            line.sep(' ', theme.win, theme.fill),
-            hl = theme.win,
-            margin = ' ',
+            -- line.sep(' ', theme.win, theme.fill),
+            hl = theme.tab,
+            ' ',
+            margin = '',
           }
         end),
-        {
-          line.sep(' ', theme.tail, theme.fill),
-          { '  ', hl = theme.tail },
-        },
+        -- {
+        --   -- line.sep(' ', theme.tail, theme.fill),
+        --   { '  ', hl = theme.tail },
+        -- },
         hl = theme.fill,
       }
     end)
