29ab69263324f52a0853849a032e143eb87f11fc
Christopher Torgalson <manager@bedlamhotel.com>
2024-01-18 09:23:54 +0100

    The underlying cause was tmux misconfiguration

--- private_dot_config/nvim/lua/custom/plugins/solarized.lua
+++ private_dot_config/nvim/lua/custom/plugins/solarized.lua
@@ -1,4 +1,21 @@
 return {
+  -- not bad, a little flat.
+  -- 'svrana/neosolarized.nvim',
+  -- dependencies = 'tjdevries/colorbuddy.nvim',
+  -- config = function ()
+  --   require('neosolarized').setup({
+  --     comment_italics = true,
+  --     background_set = true,
+  --   })
+  -- end,
+  --
+  -- Colours too flat.
+  -- "craftzdog/solarized-osaka.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- opts = {},
+  --
+  -- Colours broken after reboot (?).
   'ishan9299/nvim-solarized-lua',
   config = function()
     vim.o.background = 'dark'
