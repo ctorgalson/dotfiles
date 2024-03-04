5d701361e27fce363ec403cee8730fa1cca8b8b8
Christopher Torgalson <manager@bedlamhotel.com>
2024-03-04 11:15:40 +0100

    Delete private_dot_config/nvim/lua/custom/plugins/webc.lua
    Remove webc.lua file

--- private_dot_config/nvim/lua/custom/plugins/webc.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'bennypowers/webc.nvim',                                                                                                                                                      
-  dependencies = 'nvim-treesitter/nvim-treesitter',                                                                                                                                    
-  opts = true,                                                                                                                                                                         
-} 
