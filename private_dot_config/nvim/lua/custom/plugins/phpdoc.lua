e3cd3e0d6587cd52b08ca976228e1949c4fa0ed3
Christopher Torgalson <manager@bedlamhotel.com>
2024-03-04 11:16:09 +0100

    Delete private_dot_config/nvim/lua/custom/plugins/phpdoc.lua
    Remove phpdoc.lua file

--- private_dot_config/nvim/lua/custom/plugins/phpdoc.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'claytonrcarter/tree-sitter-phpdoc',                                                                                                                                          
-  depencencies = 'nvim-treesitter/nvim-treesitter',                                                                                                                                    
-  opts = true,                                                                                                                                                                         
-} 
